ca344685e30a838e08c9c9832cd9fabd7a881db0
Julian Rosse <julian@helixbass.net>
2021-03-02 20:10:11 -0500

    log type of enclosing chain step

--- src/index.ts
+++ src/index.ts
@@ -11,26 +11,44 @@ const init = (modules: {typescript: typeof import('typescript/lib/tsserverlibrar
         }
       }
 
+      const log = (message: string) => info.project.projectService.logger.info(message)
+      const dump = (obj: any, depth?: number) => require('util').inspect(obj, false, depth)
+
       proxy.getDefinitionAndBoundSpan = (fileName, position) => {
-        const ret = info.languageService.getDefinitionAndBoundSpan(fileName, position)
-        const def = ret!.definitions![0]!
-        info.project.projectService.logger.info("Wheeee")
-        return info.languageService.getDefinitionAndBoundSpan(fileName, position)
-      }
-
-      proxy.getCompletionsAtPosition = (fileName, position, opts) => {
-        const prior = info.languageService.getCompletionsAtPosition(fileName, position, opts);
-        if (!prior) return prior
-        const oldLength = prior.entries.length;
-        const whatToRemove = ['caller', 'getDay']
-        prior.entries = prior.entries.filter(e => whatToRemove.indexOf(e.name) < 0);
-
-        // Sample logging for diagnostic purposes
-        if (oldLength !== prior.entries.length) {
-            info.project.projectService.logger.info(`Removed ${oldLength - prior.entries.length} entries from the completion list`);
+        const existing = info.languageService.getDefinitionAndBoundSpan(fileName, position)
+        const definition = existing?.definitions?.[0]
+        if (!definition) return existing
+        const program = info.languageService.getProgram()
+        if (!program) return existing
+        const sourceFile = program.getSourceFile(fileName)
+        if (!sourceFile) return existing
+        const definitionStart = definition.textSpan.start
+        const definitionEnd = definition.textSpan.start + definition.textSpan.length
+        const findEnclosingNode = (node: ts.Node) => node.forEachChild((childNode) => {
+          if (childNode.getStart() <= definitionStart && definitionEnd <= childNode.getEnd()) return childNode
+        })
+        const expectedEnclosingNodeSyntaxKinds: ts.SyntaxKind[] = [
+          ts.SyntaxKind.VariableStatement,
+          ts.SyntaxKind.VariableDeclarationList,
+          ts.SyntaxKind.VariableDeclaration,
+          ts.SyntaxKind.CallExpression,
+        ]
+        let enclosingNode: ts.Node | undefined = sourceFile
+        for (const expectedEnclosingNodeSyntaxKind of expectedEnclosingNodeSyntaxKinds) {
+          enclosingNode = findEnclosingNode(enclosingNode)
+          if (!enclosingNode) return existing
+          log(`found ${enclosingNode.kind}: ${enclosingNode.getStart()}-${enclosingNode.getEnd()}`)
+          if (enclosingNode.kind !== expectedEnclosingNodeSyntaxKind) return existing
         }
-
-        return prior;
+        const enclosingFlowMaxNode = enclosingNode as ts.CallExpression
+        if (enclosingFlowMaxNode.expression.kind !== ts.SyntaxKind.Identifier || (enclosingFlowMaxNode.expression as ts.Identifier).escapedText !== 'flowMax') return existing
+        const enclosingChainStepNode = findEnclosingNode(enclosingFlowMaxNode)
+        if (!enclosingChainStepNode) return existing
+        const typeChecker = program.getTypeChecker()
+        const enclosingChainStepType = typeChecker.getTypeAtLocation(enclosingChainStepNode)
+        // log(`found type: symbol: ${dump(enclosingChainStepType.symbol)}, apparent properties: ${dump(enclosingChainStepType.getApparentProperties())}`)
+        log(`found type: ${typeChecker.typeToString(enclosingChainStepType)}`)
+        return existing
       };
 
       return proxy
